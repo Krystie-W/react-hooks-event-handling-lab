5c2f2e8b37831257b7e681a7065e5ecf7c942506
Krystie-W <tsukio.scoring@gmail.com>
2021-12-02 14:32:58 +1100

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function hi () {
+        console.log('Good!')
+    }
+    function lookAtMe() {
+        console.log('Hey! Eyes on me!')
+        }
+  return (
+    <div>
+        <button onFocus={() => hi()}
+        onBlur={() => lookAtMe()}
+        >Eyes on me</button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
